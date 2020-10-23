15548d72ecf5a7422065ae1c9e8041265ba21b10
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2020-10-23 20:20:47 -0300

    First Commit

---
+++ BreadCrumbBlazorBootstrap4/Data/BreadCrumbData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreadCrumbBlazorBootstrap4.Data
+{
+    public class BreadCrumbData
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+
+    }
+}
